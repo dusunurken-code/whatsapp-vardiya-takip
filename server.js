8df9b0facbfcae9f35e3e12549521bb3f8dc55ff
dusunurken-code <dusunurken@gmail.com>
2026-07-05 13:22:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-
+// Eski halini silip bunu yapıştırın:
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', 
+            '--disable-gpu'
+        ]
+    }
+});
